2d7e7ca5997279dd9dc58528748f39aa8bf9ed85
LauraOrte <laura.orte7@gmail.com>
2022-05-03 16:54:04 +0200

    rutas listas

--- client/src/router/AppRouter.js
+++ client/src/router/AppRouter.js
@@ -6,18 +6,17 @@ import {
     Redirect,
   } from "react-router-dom";
 import { ChatPage } from '../pages/ChatPage';
-import { LoginPage } from '../pages/LoginPage';
-import { RegisterPage } from '../pages/RegisterPage';
+import { AuthRouter } from './AuthRouter';
 
+//establacer rutas (principal y sencudaria)
 export const AppRouter = () => {
     return (
         <Router>
         <div>
-
+            
             <Switch>
-                <Route path="/login" component={ LoginPage} />
-                <Route path="/register" component={ RegisterPage } />
-                <Route path="/" component={ ChatPage } />
+                <Route path="/auth" component={ AuthRouter } />
+                <Route exact path="/" component={ ChatPage } />
 
                 <Redirect to="/" />
 
---
+++ client/src/router/AuthRouter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+import { LoginPage } from '../pages/LoginPage';
+import { RegisterPage } from '../pages/RegisterPage';
+
+//rutas secundarias
+export const AuthRouter = () => {
+  return (
+     <Switch>
+        <Route exact path="/auth/login" component={ LoginPage } />
+        <Route exact path="/auth/register" component={ RegisterPage } />
+
+        <Redirect to="/auth/login" />
+    </Switch>
+
+  )
+}
