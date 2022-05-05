0f4f089406fd53c3d77638047ce2a83f85071f25
LauraOrte <laura.orte7@gmail.com>
2022-05-05 18:57:39 +0200

    avatar nuevo

--- client/src/components/SideBarChatitem.js
+++ client/src/components/SideBarChatitem.js
@@ -6,7 +6,7 @@ export const SideBarChatitem = () => {
      <div className="chat_list">
      <div className="chat_people">
          <div className="chat_img"> 
-             <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" />
+             <img src="https://st2.depositphotos.com/22919514/47561/v/380/depositphotos_475610056-stock-illustration-avatar-surfer-outline-colored-icon.jpg?forcejpeg=true" alt="sunil" />
          </div>
          <div className="chat_ib">
              <h5>Some random name</h5>
