f0434514201d982c5fd0bc6b3c55a61511ae98ac
LauraOrte <laura.orte7@gmail.com>
2022-05-09 11:52:11 +0200

    fecha y hora en español

--- client/src/components/IncomingMessages.js
+++ client/src/components/IncomingMessages.js
@@ -3,7 +3,7 @@ import { horaMes } from '../helpers/horaMes'
 
 export const IncomingMessages = ({ msg }) => {
 
-    horaMes(msg.createdAt);
+    
 
   return (
     
@@ -14,7 +14,7 @@ export const IncomingMessages = ({ msg }) => {
     <div className="received_msg">
         <div className="received_withd_msg">
             <p>{ msg.mensaje }</p>
-            <span className="time_date"> 11:01 AM | June 9</span>
+            <span className="time_date"> { horaMes(msg.createdAt)} </span>
         </div>
     </div>
 </div>
